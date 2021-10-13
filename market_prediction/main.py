f61818761cd6efc482af141ed032eaf2ec723e44
Karl Bittner <karl.be.bittner@gmail.com>
2021-10-13 22:16:48 +0200

    :pencil: Added rqments.txt and basic structure

--- market_prediction/main.py
+++ market_prediction/main.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3.10
 # Created by ...
+__version__ = 0.01
 
+def MarketPrediction():
+    print('Hello World')
 
+#DB
+
+#Logging 
+
+#GUI
+
+if __name__ == '__main__':
+    MarketPrediction()
--- market_prediction/setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
-
-setup(name='Market Predictions',
-      version='0.1',
-      description='WP_WS21_Project',
-      packages=['', ''],
-     )
