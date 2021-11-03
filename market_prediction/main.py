1f43cc6de57ab7aa70c700c8f845a88ffc9e5d91
Jonas Liebermann <jonas_liebermann@web.de>
2021-11-03 17:18:52 +0100

    fix working example

--- market_prediction/main.py
+++ market_prediction/main.py
@@ -1,25 +1,28 @@
-#!/usr/bin/env python3.10
+#!/usr/bin/env python3
 # Created by ...
 __version__ = 0.01
 
 import logging 
 from configparser import ConfigParser
+import os
+import gui.gui
 
 def MarketPrediction():
     print('Market Prediction is starting up...')
     logging.info('Market Prediction started')
+    gui()
 
 #CONFIG
 config = ConfigParser()
-config.read("../config.ini")
+config.read(os.getcwd() + "\config.ini")
+
 
     
 #DB
 
 #Logging
 SERVERCONFIG = config["SERVERCONFIG"]
-logging.basicConfig(filename='SERVERCONFIG,["LOGGING_LOCATION"]', encoding='utf-8', level=logging.SERVERCONFIG,["LOGGING_LEVEL"])
-print (SERVERCONFIG)
+logging.basicConfig(filename=SERVERCONFIG["LOGGING_LOCATION"], encoding='utf-8', level=SERVERCONFIG["LOGGING_LEVEL"])
 
 #GUI
 
