02fb78b81cb33edee92e3afaad889a43b7b6bedc
Thomas <th.raffel@gmail.com>
2021-11-26 12:06:03 +0100

    erweiterung des predicition algorithmus

--- market_prediction/finance/prediction.py
+++ market_prediction/finance/prediction.py
@@ -1,13 +1,16 @@
+# import necessary libraries
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout, Bidirectional
 from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
+# python scraper of yahoo finance (not a reliable API)
 from yahoo_fin import stock_info as si
 from collections import deque
 
 import os
+import time #needed for Model Training
 import numpy as np
 import pandas as pd
 import random
@@ -17,6 +20,10 @@ np.random.seed(314)
 tf.random.set_seed(314)
 random.seed(314)
 
+#-----------------------------------------------------------------------------------------------------------------------
+#Preparing our Dataset
+#-----------------------------------------------------------------------------------------------------------------------
+
 def shuffle_in_unison(a, b):
     # shuffle two arrays in the same way
     state = np.random.get_state()
@@ -24,6 +31,7 @@ def shuffle_in_unison(a, b):
     np.random.set_state(state)
     np.random.shuffle(b)
 
+#long function but flexible
 def load_data(ticker, n_steps=50, scale=True, shuffle=True, lookup_step=1, split_by_date=True,
                 test_size=0.2, feature_columns=['adjclose', 'volume', 'open', 'high', 'low']):
     """
@@ -48,13 +56,17 @@ def load_data(ticker, n_steps=50, scale=True, shuffle=True, lookup_step=1, split
         df = ticker
     else:
         raise TypeError("ticker can be either a str or a `pd.DataFrame` instances")
+    
     # this will contain all the elements we want to return from this function
     result = {}
+
     # we will also return the original dataframe itself
     result['df'] = df.copy()
+
     # make sure that the passed feature_columns exist in the dataframe
     for col in feature_columns:
         assert col in df.columns, f"'{col}' does not exist in the dataframe."
+    
     # add date as a column
     if "date" not in df.columns:
         df["date"] = df.index
@@ -67,15 +79,19 @@ def load_data(ticker, n_steps=50, scale=True, shuffle=True, lookup_step=1, split
             column_scaler[column] = scaler
         # add the MinMaxScaler instances to the result returned
         result["column_scaler"] = column_scaler
+    
     # add the target column (label) by shifting by `lookup_step`
     df['future'] = df['adjclose'].shift(-lookup_step)
     # last `lookup_step` columns contains NaN in future column
+
     # get them before droping NaNs
     last_sequence = np.array(df[feature_columns].tail(lookup_step))
+    
     # drop NaNs
     df.dropna(inplace=True)
     sequence_data = []
     sequences = deque(maxlen=n_steps)
+
     for entry, target in zip(df[feature_columns + ["date"]].values, df['future'].values):
         sequences.append(entry)
         if len(sequences) == n_steps:
@@ -85,13 +101,16 @@ def load_data(ticker, n_steps=50, scale=True, shuffle=True, lookup_step=1, split
     # this last_sequence will be used to predict future stock prices that are not available in the dataset
     last_sequence = list([s[:len(feature_columns)] for s in sequences]) + list(last_sequence)
     last_sequence = np.array(last_sequence).astype(np.float32)
+
     # add to result
     result['last_sequence'] = last_sequence
+
     # construct the X's and y's
     X, y = [], []
     for seq, target in sequence_data:
         X.append(seq)
         y.append(target)
+    
     # convert to numpy arrays
     X = np.array(X)
     y = np.array(y)
@@ -110,13 +129,148 @@ def load_data(ticker, n_steps=50, scale=True, shuffle=True, lookup_step=1, split
         # split the dataset randomly
         result["X_train"], result["X_test"], result["y_train"], result["y_test"] = train_test_split(X, y, 
                                                                                 test_size=test_size, shuffle=shuffle)
+    
     # get the list of test set dates
     dates = result["X_test"][:, -1, -1]
+   
     # retrieve test features from the original dataframe
     result["test_df"] = result["df"].loc[dates]
+   
     # remove duplicated dates in the testing dataframe
     result["test_df"] = result["test_df"][~result["test_df"].index.duplicated(keep='first')]
+   
     # remove dates from the training/testing sets & convert to float32
     result["X_train"] = result["X_train"][:, :, :len(feature_columns)].astype(np.float32)
     result["X_test"] = result["X_test"][:, :, :len(feature_columns)].astype(np.float32)
     return result
+
+#----------------------------------------------------------------------------------------------------------------------------------------
+# Model Creation
+#----------------------------------------------------------------------------------------------------------------------------------------
+# RNN with Keras
+# flexible model
+# number of layers, dropout rate, rnn cell, loss and the optimizer can be changed
+# function creates an RNN (Recurrent neural network)
+# RNNs are neural nets especially usefull for modelling sequence data like time series
+# Keras is designed for its simplicity and easy configuration
+def create_model(sequence_length, n_features, units=256, cell=LSTM, n_layers=2, dropout=0.3,
+                loss="mean_absolute_error", optimizer="rmsprop", bidirectional=False):
+    model = Sequential()
+    for i in range(n_layers):
+        if i == 0:
+            # first layer
+            if bidirectional:
+                model.add(Bidirectional(cell(units, return_sequences=True), batch_input_shape=(None, sequence_length, n_features)))
+            else:
+                model.add(cell(units, return_sequences=True, batch_input_shape=(None, sequence_length, n_features)))
+        elif i == n_layers - 1:
+            # last layer
+            if bidirectional:
+                model.add(Bidirectional(cell(units, return_sequences=False)))
+            else:
+                model.add(cell(units, return_sequences=False))
+        else:
+            # hidden layers
+            if bidirectional:
+                model.add(Bidirectional(cell(units, return_sequences=True)))
+            else:
+                model.add(cell(units, return_sequences=True))
+        # add dropout after each layer
+        model.add(Dropout(dropout))
+    model.add(Dense(1, activation="linear"))
+    model.compile(loss=loss, metrics=["mean_absolute_error"], optimizer=optimizer)
+    return model
+
+#----------------------------------------------------------------------------------------------------------------------------------------
+# Model Training
+#----------------------------------------------------------------------------------------------------------------------------------------
+
+# Window size or the sequence length
+N_STEPS = 50
+
+# Lookup step, 1 is the next day
+LOOKUP_STEP = 15
+
+# whether to scale feature columns & output price as well
+SCALE = True
+scale_str = f"sc-{int(SCALE)}"
+
+# whether to shuffle the dataset
+SHUFFLE = True
+shuffle_str = f"sh-{int(SHUFFLE)}"
+
+# whether to split the training/testing set by date
+SPLIT_BY_DATE = False
+split_by_date_str = f"sbd-{int(SPLIT_BY_DATE)}"
+
+# test ratio size, 0.2 is 20%
+TEST_SIZE = 0.2
+
+# features we use to predict the next price value
+FEATURE_COLUMNS = ["adjclose", "volume", "open", "high", "low"]
+
+# date now
+date_now = time.strftime("%Y-%m-%d")
+
+### model parameters
+# number of RNN layers we want to use
+N_LAYERS = 2
+
+# RNN cell to use, default is LSTM cell
+CELL = LSTM
+
+# 256 LSTM neurons
+UNITS = 256
+
+# 40% dropout
+DROPOUT = 0.4
+
+# whether to use bidirectional RNNs
+BIDIRECTIONAL = False
+
+### training parameters
+LOSS = "huber_loss" # loss function used for the regression problem
+OPTIMIZER = "adam" # optimizer algorithm
+BATCH_SIZE = 64 # number of data samples used in each training iteration
+EPOCHS = 500 # number of times the algorithm will pass through the entire training set; higher number is recommended
+
+# Amazon stock market
+ticker = "AMZN"
+ticker_data_filename = os.path.join("data", f"{ticker}_{date_now}.csv")
+
+# model name to save, making it as unique as possible based on parameters
+model_name = f"{date_now}_{ticker}-{shuffle_str}-{scale_str}-{split_by_date_str}-\
+{LOSS}-{OPTIMIZER}-{CELL.__name__}-seq-{N_STEPS}-step-{LOOKUP_STEP}-layers-{N_LAYERS}-units-{UNITS}"
+if BIDIRECTIONAL:
+    model_name += "-b"
+
+# create these folders if they does not exist
+if not os.path.isdir("results"):
+    os.mkdir("results")
+if not os.path.isdir("logs"):
+    os.mkdir("logs")
+if not os.path.isdir("data"):
+    os.mkdir("data")
+
+#---------------------------------------------------------------------------------------------------------------------
+# load the data
+data = load_data(ticker, N_STEPS, scale=SCALE, split_by_date=SPLIT_BY_DATE, 
+                shuffle=SHUFFLE, lookup_step=LOOKUP_STEP, test_size=TEST_SIZE, 
+                feature_columns=FEATURE_COLUMNS)
+# save the dataframe
+data["df"].to_csv(ticker_data_filename)
+# construct the model
+model = create_model(N_STEPS, len(FEATURE_COLUMNS), loss=LOSS, units=UNITS, cell=CELL, n_layers=N_LAYERS,
+                    dropout=DROPOUT, optimizer=OPTIMIZER, bidirectional=BIDIRECTIONAL)
+# some tensorflow callbacks
+checkpointer = ModelCheckpoint(os.path.join("results", model_name + ".h5"), save_weights_only=True, save_best_only=True, verbose=1)
+tensorboard = TensorBoard(log_dir=os.path.join("logs", model_name))
+# train the model and save the weights whenever we see 
+# a new optimal model using ModelCheckpoint
+history = model.fit(data["X_train"], data["y_train"],
+                    batch_size=BATCH_SIZE,
+                    epochs=EPOCHS,
+                    validation_data=(data["X_test"], data["y_test"]),
+                    callbacks=[checkpointer, tensorboard],
+                    verbose=1)
+
